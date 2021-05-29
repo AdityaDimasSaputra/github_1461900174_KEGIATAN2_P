4889cd622b7ec1e5af3b722c270ffa8a28ae42c8
AdityaDimasSaputra <adityadmss@gmail.com>
2021-05-29 14:45:51 +0700

    Mengerjakan Praktikum

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\controllerdimas;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/ms_buku',[controllerdimas::class,'ms_buku']);
